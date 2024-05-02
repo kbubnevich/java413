5bbbbfe06ea2b7a1093b098f2906253c3cb0bbbc
kbubnevich <99471336+kbubnevich@users.noreply.github.com>
2024-05-02 09:39:37 +0700

    Delete src directory

--- src/lab1/Zadanie1.java
+++
@@ -1,7 +0,0 @@
-package lab1;
-
-public class Zadanie1 {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld");
-    }
-}
--- src/lab1/Zadanie2.java
+++
@@ -1,9 +0,0 @@
-package lab1;
-
-import java.util.Scanner;
-
-public class Zadanie2 {
-    public static void main(String[] args) {
-
-    }
-}
